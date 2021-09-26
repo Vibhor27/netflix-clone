cd662191e5347c9b98f5f0b2bf5aa7a38e5a3d6d
DESKTOP-0VMHV97\Vibhor Dhawan <vibhoralfa@gmail.com>
2021-09-26 21:59:48 +0530

    Check in

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY="161eaeffe78cab30def02bc1a65a06a0";
+
+const requests={
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&langugage=en-us`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&langugage=en-us`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests;
