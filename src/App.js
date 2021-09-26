afe9c5570e328a0cdae5630be5c4535cd97e8708
DESKTOP-0VMHV97\Vibhor Dhawan <vibhoralfa@gmail.com>
2021-09-26 22:58:28 +0530

    Top rated Movies

--- src/App.js
+++ src/App.js
@@ -13,12 +13,7 @@ function App() {
 
      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow={true}/>
-     <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-     <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-     <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-     <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-     <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies}/>
-     <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+     
     </div>
   );
 }
